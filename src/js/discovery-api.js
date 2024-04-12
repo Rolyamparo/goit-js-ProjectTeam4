f9340f491cf81c86b626d08f59bf189d3c6d594d
Rolyamparo <143687528+Rolyamparo@users.noreply.github.com>
2024-04-12 21:23:54 +0800

    initial commit

--- src/js/discovery-api.js
+++ src/js/discovery-api.js
@@ -1,6 +1,6 @@
 export const
   // BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=71YE9oAJEA4qZlqXbW37iGIiChRCAVD4&classificationName=music";
-  BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=71YE9oAJEA4qZlqXbW37iGIiChRCAVD4";
+  BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json?";
 const API_KEY = '71YE9oAJEA4qZlqXbW37iGIiChRCAVD4';
 
 export const options = {
